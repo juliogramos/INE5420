901be48cf66d318df95cf2ecfe9cdf1a654e903c
juliogramos <juliogoncalvesramos@yahoo.com.br>
2023-03-21 14:01:46 -0300

    zoom funcionando eu espero, panning não

--- object.py
+++ object.py
@@ -7,10 +7,6 @@ class Point:
     name: str
     type: str = "Point"
 
-    def draw(self, painter, x , y):
-        painter.drawPoint(x, y)
-        print("paintou")
-
 @dataclass
 class Line:
     p1: Point
@@ -18,16 +14,8 @@ class Line:
     name: str
     type: str = "Line"
 
-    def draw(self, painter):
-        painter.drawLine(self.p1.x, self.p1.y, self.p2.x, self.p2.y)
-
 @dataclass
 class Wireframe:
     points: list[Point]
     name: str
     type: str = "Polygon"
-
-    def draw(self, painter):
-        for i in range(1, len(self.points)):
-            Line(self.points[i-1], self.points[i], "").draw(painter)
-        Line(self.points[-1], self.points[0], "").draw(painter)
